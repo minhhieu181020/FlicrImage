4bfd984e83b5c063d5bb32275ee31f7423e0d0ec
minhhieu181020 <nguyenminhhieu181020@gmail.com>
2025-03-13 21:54:10 +0700

    check push

---
+++ FlicrImage/ViewController/MainScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlicrImage
+//
+//  Created by Nguyễn Minh Hiếu on 13/03/2025.
+//
+
+import UIKit
+
+class MainScreenViewController: ViewController {
+    lazy var viewModel_: MainScreenViewModel = self.viewModel(MainScreenViewModel.self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("check git")
+    }
+    
+
+
+}
+
